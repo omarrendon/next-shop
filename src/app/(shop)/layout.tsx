c158ebc676ef92b242f7bf197d3d81c9ef55392e
Omar Rendon <omarcr.96@gmail.com>
2024-10-30 01:03:30 -0600

    COMPONET: SideBar

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { TopMenu } from "@/components";
+import { SideBar, TopMenu } from "@/components";
 
 export default function shopLayout({
   children,
@@ -8,6 +8,7 @@ export default function shopLayout({
   return (
     <main className="min-h-screen ">
       <TopMenu />
+      <SideBar />
       <div className="px-0 sm:px-10">{children}</div>
     </main>
   );
