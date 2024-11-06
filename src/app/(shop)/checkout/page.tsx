492349dab4cfcc14b6dd434c600c57dd2fa2dcdd
Omar Rendon <omarcr.96@gmail.com>
2024-11-06 01:01:08 -0600

    Checkout page its done

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,5 +1,100 @@
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-export default function CheckOutPage() {
-  return <div>CheckOutPage</div>;
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+export default function CheckoutPage() {
+  return (
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verificar orden" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar productos</span>
+            <Link href={"/cart"} className="underline mb-5">
+              Editar carrito
+            </Link>
+
+            {productsInCart.map(product => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price} X 3</p>
+                  <p className="font-bold">Subtotal: ${product.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* cHECKOUT CARD */}
+          <div className="bg-white rounded-xl shadow-xl p-7 ">
+            <h2 className="text-2xl font-bold mb-2">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p className="text-xl">Omar Rendón</p>
+              <p>Av. Siempre viva 123</p>
+              <p>Col. Centro</p>
+              <p>Ciudad de México</p>
+              <p>123.123.123</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+
+            <h2 className="text-2xl mb-2">Resumén de orden</h2>
+            <div className="grid  grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right"> 3 articulos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$100.00</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right"></span>
+
+              <span className="mt-5 text-2xl">Total: </span>
+              <span className="mt-5 text-2xl text-right">$100.00</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <p className="mb-5">
+                <span className="text-xs">
+                  Al hacer clic en "Colocar orden", aceptas nuestros{" "}
+                  <a href="#" className="underline">
+                    términos y condiciones
+                  </a>{" "}
+                  y{" "}
+                  <a href="#" className="underline">
+                    política de privacidad
+                  </a>
+                </span>
+              </p>
+              <Link
+                className="flex btn-primary justify-center"
+                href={"/orders/123"}
+              >
+                Colocar orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
