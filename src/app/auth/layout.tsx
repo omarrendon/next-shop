6a2792496c0233d92778ff51d2f57bc3d188f9b5
Omar Rendon <omarcr.96@gmail.com>
2024-11-07 00:26:34 -0600

    PAGES: Login and NewAccount created

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,5 +3,9 @@ export default function shopLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <main>{children}</main>;
+  return (
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">{children}</div>
+    </main>
+  );
 }
