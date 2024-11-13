df9b663f3ff03cd7736b673c55ea0ec2ddcc47cf
Omar Rendon <omarcr.96@gmail.com>
2024-11-12 19:24:51 -0600

    pagination server side

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -2,9 +2,23 @@
 
 import prisma from "@/lib/prisma";
 
-export const getpaginatedProductsWithImages = async () => {
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getpaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
   try {
+    // request to fecth products
     const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
       include: {
         ProductImage: {
           take: 2,
@@ -14,11 +28,15 @@ export const getpaginatedProductsWithImages = async () => {
         },
       },
     });
+    // Getting total pages
+    const totalCount = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCount / take);
     return {
+      currentPage: page,
+      totalPages: totalPages,
       products: products.map(product => ({
         ...product,
         images: product.ProductImage.map(image => image.url),
-        // type: product.
       })),
     };
   } catch (error) {
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,24 @@
+import { redirect } from "next/navigation";
+
 import { getpaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 
-export default async function Homepage() {
-  const { products } = await getpaginatedProductsWithImages();
-  console.log({ products });
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function Homepage({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products, currentPage, totalPages } =
+    await getpaginatedProductsWithImages({
+      page,
+    });
+
+  if (products.length === 0) {
+    redirect("/");
+  }
 
   return (
     <>
